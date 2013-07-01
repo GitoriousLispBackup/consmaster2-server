038d505cd3f8a26e95fc8f02f88331d61f916ae0
davidcal123 <dcalmeille@gmail.com>
2013-07-01 12:18:31 +0100

    usage client / serveur

---
+++ cltSrv.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# COURS   : EDF4REPA - IED - Paris 8
+# PROJET  : consMaster2
+# AUTEUR  : David Calmeille - 11299110@foad.iedparis8.net - L2
+# FICHIER : cltSrv.py
+# CONTENU : client : Exemples d'utilisation de dialogues client / serveur
+# VERSION : 0.1
+# LICENCE : GNU
+
+import json
+from connexion import Connexion
+from codes import *
+
+
+#////////////////////////////////////////////////////////////////////////////
+#   USAGE: UTILISATEURS
+#////////////////////////////////////////////////////////////////////////////
+
+#Creation d'un nouvel utilisateur
+#data = '{"action":"creatUser","data":{"nom": "Zola", "prenom":"Emile", "password":"emzo123", "droit":1}}'
+
+# Identification d'un utilisateur
+#data = '{"action":"identUser","data":{"nom": "Zola", "password":"emzo123"}}'
+
+# Suppression d'un utilisateur
+#data = '{"action":"delUser","data":{"id": 3}}'
+
+# Listing des utilisateurs
+data = '{"action":"listUser","data":{"prenom":"Emile","droit":1}}'
+
+
+
+#////////////////////////////////////////////////////////////////////////////
+#   USAGE: EXERCICES
+#////////////////////////////////////////////////////////////////////////////
+
+#Creation d'un nouvel exercice
+#data = '{"action":"creatExo","data":{"level": 2, "type": "__NG__", "__ExoBase__": true, "lst": ["(1 2 (3 . 4))"]}}'
+
+# Loading des exercices
+#data = '{"action":"loadExo","data":{"type":"__GN__","__ExoBase__":1, "level":1}}'
+
+# Suppression d'un exercice
+#data = '{"action":"delExo","data":{"id": 6}}'
+
+#////////////////////////////////////////////////////////////////////////////
+
+new_s = Connexion(data)
+
+try:
+    print(new_s.erreur)
+except AttributeError:
+    print (new_s.result)
+
+#dataSon = json.loads(new_s.result)
+#print(dataSon["data"])
+
+print ("La suite...")
