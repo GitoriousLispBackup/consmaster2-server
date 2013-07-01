391c0a15bf23cf556b1ce10b404429e124b31b05
davidcal123 <dcalmeille@gmail.com>
2013-07-01 12:19:23 +0100

    codes.py

---
+++ codes.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# COURS   : EDF4REPA - IED - Paris 8
+# PROJET  : consMaster2
+# AUTEUR  : David Calmeille - 11299110@foad.iedparis8.net - L2
+# FICHIER : code.py
+# CONTENU : codes
+# VERSION : 0.1
+# LICENCE : GNU
+
+
+# Connexion au serveur
+HOST = "localhost"
+PORT = 9992
+
+# Code erreur
+E_AUC = "Erreur: creatUser"
+E_AUI = "Erreur: identUser"
+E_AUD = "Erreur: delUser"
+E_AUL = "Erreur: listtUser"
+
+E_AEC = "Erreur: creatExo"
+E_AEL = "Erreur: loadExo"
+E_AED = "Erreur: delExo"
+
+# Compression
+COMP = "Off"
+
+
+
+
+
+
+
+
+
+
+
+
+
